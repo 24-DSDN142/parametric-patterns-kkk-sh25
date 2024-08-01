bca87885dfd2b88d01cdc22be4fe42b9496a79e6
kkk-sh25 <yangagkrisha@gmail.com>
2024-08-01 19:31:03 +1200

    commit

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -8,31 +8,30 @@ let centerY = canvas_height/2;//drawing position
 //CAT VARIABLES
 let cat_head = 45; // Size of the cat head
 let eye_Size = 18; // size of the eyes
-let pupil_width = 10; // size of the pupil 
-let pupil_height = 10; // height size of the pupil
-let mouth_Size = 0; // mouth size
+let pupil_width = 9; // size of the pupil 
+let pupil_height = 5; // height size of the pupil
+let mouth_Size = 10; // mouth size
 
 let star = 29.5; // Star Size
-let moon_size = 50; // Moon size
-let heart_size = 50;
-
+let moon_size = 55; // Moon size
+let heart_size = 70;
 
 //COLOURS
-let eyeColor = [255,228,196];
-let pupilColor = [66, 64, 64];
 let cloudColor = [96, 168, 219];
 let circleColor = [212, 107, 102];
-let heartColor = [244,164,96];
-let mouth_color = [168, 101, 91];
-let moon_cover = [62, 98, 171];
-let star_color = [222,184,135];
+let heartColor = [217, 129, 56];
+let eyeColor = [232, 220, 116];
+let pupilColor = [66, 64, 64];
+let mouth_color = [225];
+let moon_cover = [219, 210, 204];
+let star_color = [230, 198, 96];
 
 //IF Statements
 // Symbol type (1 for stars and circle, 2 for heart and circles, 3 for heart and stars)
-let symbolType = 3; // Change this to switch  between stars,circles, and heart
+let symbolType = 2; // Change this to switch  between stars,circles, and heart
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);//GRID_WALLPAPER DEVELOP_GLYPH
+  pWallpaper.output_mode(GRID_WALLPAPER);//GRID_WALLPAPER DEVELOP_GLYPH
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
@@ -40,30 +39,43 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
   pWallpaper.grid_settings.row_offset  = 50;
-
 }
 function wallpaper_background() {
-  background(62, 98, 171); //beige
-}
+  background(219, 210, 204); //light honeydew green colour
+  // Draw diagonal lines
+  stroke(180); // Light gray color for diagonal lines
+  strokeWeight(1);
+  
+  // Diagonal lines from top-left to bottom-right
+  for (let i = -width; i < width + height; i += 20) {
+    line(i, 0, 0, i);
+  }
 
-function drawHeart(x, y, size) {
-  fill(heartColor);
-  noStroke();
-  beginShape();
-  vertex(x, y);
-  bezierVertex(x - size / 2, y - size / 2, x - size / 2, y + size / 2, x, y + size / 2);
-  bezierVertex(x + size / 2, y + size / 2, x + size / 2, y - size / 2, x, y);
-  endShape(CLOSE);
+  // Diagonal lines from top-right to bottom-left
+  for (let i = -width; i < width + height; i += 20) {
+    line(width - i, 0, width, i);
+  }
 }
-
-function drawMoon(x, y, size) {
-  fill(circleColor);
-  noStroke();
-  ellipse(x, y, size, size);
-}
-
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   
+  // Draw heart
+  function drawHeart(centerX, centerY, heart_size) {
+    fill(heartColor);
+    noStroke();
+    beginShape();
+    vertex(centerX, centerY);
+    bezierVertex(centerX - heart_size / 2, centerY - heart_size / 2, centerX - heart_size / 2, centerY + heart_size / 2, centerX, centerY + heart_size / 2);
+    bezierVertex(centerX + heart_size / 2, centerY + heart_size / 2, centerX + heart_size / 2, centerY - heart_size / 2, centerX, centerY);
+    endShape(CLOSE);
+  }
+
+  // Draw moon
+  function drawMoon(centerX, centerY, moon_size) {
+    fill(circleColor);
+    noStroke();
+    ellipse(centerX, centerY, moon_size, moon_size);
+  }
+
   //cat head
   noStroke();
   fill(66, 64, 64);
@@ -87,7 +99,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   triangle(centerX+centerX/4.5,centerY-centerY/2.5,centerX+13,centerY-centerY/3.3,centerX+21,centerY-20);
   triangle(centerX-centerX/4.5 ,centerY-centerY/2.5,centerX-13,centerY-centerY/3.3,centerX-21,centerY-20);
 
-   //eyes
+  //eyes
   fill(eyeColor);
   noStroke();
   circle(centerX-10, centerY-15,eye_Size);//left eye
@@ -100,12 +112,10 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   //whiskers
   strokeWeight(1);
   stroke(66, 64, 64);
-  //stroke(225);
   //left whisker
   line(centerX-30, centerY-4, centerX-5, centerY-3);
   line(centerX-30, centerY+2, centerX-5, centerY-3);
   line(centerX-30, centerY+8, centerX-5, centerY-3);
-
   //right whiskers
   line(centerX+30, centerY-4, centerX+5, centerY-3);
   line(centerX+30, centerY+2, centerX+5, centerY-3);
@@ -113,8 +123,6 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   
   //nose
   fill(225);
-  //noStroke();
-  //circle(centerX, centerY+8,9);
   triangle(centerX-2,centerY-8,centerX,centerY-4,centerX+2,centerY-8)
 
   //mouth
@@ -122,9 +130,9 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   arc(centerX, centerY-2, mouth_Size, mouth_Size, 360, 180);
 
   if (symbolType === 1) { // star and circles
-    // Draw stars at the corners
+  // Draw stars at the corners
   noStroke();
-    // Middle Left
+  // Middle Left
   fill(star_color);
   ellipse(centerX - 73, centerY - 60, star + 15, star + 18); // Star
   fill(moon_cover);
@@ -133,7 +141,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX - 56, centerY - 80, star + 4, star + 13); // Cover
   ellipse(centerX - 56, centerY - 40, star + 4, star + 13); // Cover
     
-    // Bottom Right
+ // Bottom Right
   fill(star_color);
   ellipse(centerX + 73, centerY + 63, star + 20, star + 25); // Star
   fill(moon_cover);
@@ -142,7 +150,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX + 55, centerY + 84, star + 8, star + 16); // Cover
   ellipse(centerX + 55, centerY + 41, star + 8, star + 16); // Cover
   
-    // Top Right
+  // Top Right
   fill(star_color);
   ellipse(centerX + 50, centerY - 75, star, star); // Star
   fill(moon_cover);
@@ -151,7 +159,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX + 35, centerY - 60, star, star); // Cover
   ellipse(centerX + 65, centerY - 90, star, star); // Cover
     
-    // Mid Left
+  // Mid Left
   fill(star_color);
   ellipse(centerX - 70, centerY - 10, star, star); // Star
   fill(moon_cover);
@@ -160,7 +168,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX - 55, centerY - 25, star, star); // Cover
   ellipse(centerX - 55, centerY + 5, star, star); // Cover
   
-    // Bottom Left
+  // Bottom Left
   fill(star_color);
   ellipse(centerX - 50, centerY + 50, star + 10, star + 15); // Star
   fill(moon_cover);
@@ -169,7 +177,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX - 38, centerY + 70, star, star + 10); // Cover
   ellipse(centerX - 65, centerY + 70, star, star + 10); // Cover
   
-    // Mid Right
+  // Mid Right
   fill(star_color);
   ellipse(centerX + 70, centerY - 20, star, star); // Star
   fill(moon_cover);
@@ -177,18 +185,6 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX + 85, centerY - 5, star, star); // Cover
   ellipse(centerX + 55, centerY - 35, star, star); // Cover
   ellipse(centerX + 55, centerY - 5, star, star); // Cover
-
-  //moon and star
-  // drawMoon(centerX - 73, centerY - 70, moon_size / 2);
-  // drawMoon(centerX - 70, centerY + 80, moon_size / 3);
-  // drawMoon(centerX + 70, centerY - 20, moon_size / 3);
-  // drawMoon(centerX - 50, centerY + 50, moon_size / 4);
-  // drawMoon(centerX + 50, centerY - 75, moon_size / 2);
-  // drawMoon(centerX + 73, centerY + 63, moon_size / 4);
-  // drawMoon(centerX - 90, centerY - 40, moon_size / 5);
-  // drawMoon(centerX + 70, centerY + 30, moon_size / 5);
-  // drawMoon(centerX - 75, centerY + 20, moon_size / 3); 
-
 
   drawMoon(centerX - 10, centerY - 70, moon_size / 2);
   drawMoon(centerX - 85, centerY + 80, moon_size / 3);
@@ -225,9 +221,9 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   }
 
   else if (symbolType ===3) { // star and circles
-    // Draw stars at the corners
+  // Draw stars in the corners
   noStroke();
-    // Middle Left
+  // Middle Left
   fill(star_color);
   ellipse(centerX - 73, centerY - 60, star + 15, star + 18); // Star
   fill(moon_cover);
@@ -245,7 +241,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX + 55, centerY + 84, star + 8, star + 16); // Cover
   ellipse(centerX + 55, centerY + 41, star + 8, star + 16); // Cover
   
-    // Top Right
+  // Top Right
   fill(star_color);
   ellipse(centerX + 50, centerY - 75, star, star); // Star
   fill(moon_cover);
@@ -254,7 +250,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX + 35, centerY - 60, star, star); // Cover
   ellipse(centerX + 65, centerY - 90, star, star); // Cover
     
-    // Mid Left
+  // Mid Left
   fill(star_color);
   ellipse(centerX - 70, centerY - 10, star, star); // Star
   fill(moon_cover);
@@ -263,7 +259,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX - 55, centerY - 25, star, star); // Cover
   ellipse(centerX - 55, centerY + 5, star, star); // Cover
   
-    // Bottom Left
+  // Bottom Left
   fill(star_color);
   ellipse(centerX - 50, centerY + 50, star + 10, star + 15); // Star
   fill(moon_cover);
@@ -272,7 +268,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(centerX - 38, centerY + 70, star, star + 10); // Cover
   ellipse(centerX - 65, centerY + 70, star, star + 10); // Cover
   
-    // Mid Right
+  // Mid Right
   fill(star_color);
   ellipse(centerX + 70, centerY - 20, star, star); // Star
   fill(moon_cover);
@@ -297,30 +293,30 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   stroke(66, 64, 64);
   strokeWeight(4);
   curve(
-    centerX-30, centerY+20,    // Start point
-    centerX+15, centerY+55,    // Control point 1
+    centerX - 30, centerY + 20,    // Start point
+    centerX + 15, centerY + 55,    // Control point 1
     centerX + 50, centerY,    // Control point 2
     centerX + 250, centerY + 0     // End point
   );
-  curve(
+  curve( //tail2
     centerX, centerY,    // Start point
-    centerX+50, centerY+20,    // Control point 1
+    centerX + 50, centerY + 20,    // Control point 1
     centerX + 51, centerY,    // Control point 2
-    centerX-200, centerY     // End point
-  );//tail2
+    centerX - 200, centerY     // End point
+  );
 
   //Clouds
   noStroke();
   fill(cloudColor);
-  ellipse(centerX,centerY+70,centerX-45,centerY-45);//main part of the cloud
-  ellipse(centerX-20,centerY+50,centerX/4.8,centerY/6);
-  ellipse(centerX+20,centerY+50,centerX/5,centerY/6,);
-  ellipse(centerX-20,centerY+63,centerX-54,centerY-78);
-  ellipse(centerX-25,centerY+78,centerX-30,centerY-70);
-  ellipse(centerX-20,centerY+90,centerX/5,centerY/9);
-  ellipse(centerX+20,centerY+63,centerX-54,centerY-70);
-  ellipse(centerX+25,centerY+78,centerX-30,centerY-70);
-  ellipse(centerX+25,centerY+90,centerX/3,centerY/9);
+  ellipse(centerX, centerY +70, centerX - 45, centerY - 45);//main part of the cloud
+  ellipse(centerX - 20,centerY + 50,centerX / 4.8, centerY / 6);
+  ellipse(centerX + 20,centerY + 50,centerX / 5, centerY / 6,);
+  ellipse(centerX - 20,centerY + 63,centerX - 54, centerY - 78);
+  ellipse(centerX - 25,centerY + 78,centerX - 30, centerY - 70);
+  ellipse(centerX - 20,centerY + 90,centerX / 5, centerY / 9);
+  ellipse(centerX + 20,centerY + 63,centerX - 54, centerY - 70);
+  ellipse(centerX + 25,centerY + 78,centerX - 30, centerY - 70);
+  ellipse(centerX + 25,centerY + 90,centerX / 3, centerY / 9);
   
  }
 
